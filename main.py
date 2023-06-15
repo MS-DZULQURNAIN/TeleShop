6e9e14cc5631bb22d570ab31a697b307f1f84699
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 22:30:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,28 +26,29 @@ START_BTN = InlineKeyboardMarkup(
         ]]
     )
 
+@Bot.on_message(filters.incoming & filters.private, group=-1)
+async def fsub(bot, message):
+  if not FSUB:
+    return True
+  try:
+    await bot.get_chat_member(FSUB, message.from_user.id)
+  except UserNotParticipant:
+    if FSUB.startswith("-100"):
+      link = await bot.export_chat_invite_link(FSUB)
+    else:
+      link = f"https://t.me/{FSUB}"
+    tfsub = f"👋Halo {message.from_user.mention}\n\nSebelum menggunakan {BOT_NAME} kamu harus subscribe atau join channel dibawah ini jika sudah klik coba lagi💡"
+    bfsub = InlineKeyboardMarkup([
+                                  [InlineKeyboardButton(text="Join Channel", url=link),],
+                                  [InlineKeyboardButton(text="Coba lagi", url=f"https://t.me/{bot.username}?start={message.command[1]}")]
+                                ])
+    await message.reply_text(tfsub, reply_markup=bfsub)
+    await message.stop_propagation()
+
 
 @Bot.on_message(filters.command(["start"]))
 async def start(bot, message):
-    if not FSUB:
-       return True
-    try:
-        try:
-            await bot.get_chat_member(FSUB, message.from_user.id)
-        except UserNotParticipant:
-            info = await bot.export_chat_invite_link(lobang.FSUB)
-            link = info.invite_link
-            bfsub = InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton(text="Join", url = link),
-                ]
-              ]
-             )
-            try:
-                await message.reply_text(
-                    f"join dulu ya tod",
-                    reply_markup=bfsub)
-    text = START_TXT.format(message.from_user.mention)
+    text = START_TXT.format(message.from_user.mention, BOT_NAME)
     reply_markup = START_BTN
     await message.reply_text(
         text=text,
