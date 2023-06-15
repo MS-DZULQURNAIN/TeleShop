86390b7aee2498065eb628c0eaa510cf84857de5
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-15 22:30:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 from pyromod import listen
-from lobang import BOT_TOKEN, API_ID, API_HASH, thumb, FSUB
+from lobang import BOT_TOKEN, API_ID, API_HASH, thumb, FSUB, BOT_NAME
 
 
 Bot = Client(
