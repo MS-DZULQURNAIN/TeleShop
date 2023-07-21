1d1f6fca02f87ccbcddc515e200da8db3445a683
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-21 12:06:40 +0700

    Update data.py

--- DzThumbnailBot/core/data.py
+++ DzThumbnailBot/core/data.py
@@ -5,3 +5,14 @@ Saya Adalah {BOT_NAME} yang akan membantu Anda untuk memasang thumbnail video de
 
 Klik button tutorial untuk mulai mengetahui cara-cara nya**
 """
+
+TUTOR_TXT = """
+**Tutorial ThumbnailRobot💡
+
+1.Kirimkan video ke ThumbnailRobot
+2.Kirimkan Thumnail/foto yg akan dijadikan Thumnail
+3.Selesai
+
+Note:
+Bot hanya bisa memasang thumbnail dengan minimal durasi 20 detik,jika masih gagal silakan ajukan keluhannya ke developer👤
+"""
