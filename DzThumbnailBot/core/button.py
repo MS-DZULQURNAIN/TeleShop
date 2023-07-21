ae1562b9cb60e52572c5daefdadb4b7a2f7b0f3f
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-21 12:10:09 +0700

    Update button.py

--- DzThumbnailBot/core/button.py
+++ DzThumbnailBot/core/button.py
@@ -3,17 +3,23 @@ from pyrogram.types import *
 
 BSTART = InlineKeyboardMarkup([
             [
-              InlineKeyboardButton(text="Developer👤", url="https://t.me/MSDZULQRNN"),
+              InlineKeyboardButton(text="Developer👤", url="https://t.me/MSDQQQ"),
             ],
             [
               InlineKeyboardButton(text="Tutorial💡", callback_data="tutor"),
               InlineKeyboardButton(text="Donasi💌", callback_data="donasi"),
             ],
             [
-              InlineKeyboardButton(text="Channel", url="https://t.me/MSPR0JECT"),
-              InlineKeyboardButton(text="Support", url="https://t.me/envSample"),
+              InlineKeyboardButton(text="Channel", url="https://t.me/DezetStore"),
+              InlineKeyboardButton(text="Support", url="https://t.me/DezetSupport"),
             ],
             [
               InlineKeyboardButton(text="Tutup", callback_data="close"),
-            ],
-        ])
+            ]
+       ])
+
+BHOME = InlineKeyboardMarkup([
+                   [
+                        InlineKeyboardButton("Kembali", callback_data="home")
+                   ]
+             ])
