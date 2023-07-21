590883055ec45bd25c7052d538d363d514fa5d87
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-21 10:44:41 +0700

    Update env.py

--- env.py
+++ env.py
@@ -2,10 +2,10 @@
 # t.me/MSPR0JECT | t.me/envSample
 # KONTOL LU CLONER
 
-API_ID = "16452568"
-API_HASH = "f936697c5c9e5bffd433babef7a4e4c9"
-BOT_TOKEN = "5883022355:AAEk6wtmdeUxyajh8fxBFfjcQdiLtfQ-f3M"
-FSUB = "MSPR0JECT"
-thumb = "https://telegra.ph/file/bdf23d4e78c8337249c26.png"
-BOT_NAME = "ThumbnailRobot"
-CHANNEL_ID = "-1001938323171"
+API_ID = 
+API_HASH = 
+TOKEN =
+BOT_NAME =
+OWNER_ID =
+LOG = 
+DATABASE_URI =
