82bf8845e8f830847cfdbcb91e8fa6fe82384eff
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-21 08:41:23 +0700

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -7,13 +7,8 @@ database = dbclient["DzThumbnailBot"]
 
 
 user_data = database['users']
-prem_data = database ['prem']
 
 
-
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
-    return bool(found)
 
 async def add_user(user_id: int):
     user_data.insert_one({
@@ -30,19 +25,10 @@ async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
 
-async def add_prem(user_id: int):
-    prem_data.insert_one({'_id': user_id})
+async def prem(user_id: int):
+    user_data.find_one({'_id': user_id}, {'$set': {'prem': True}})
     return
 
-async def del_prem(user_id: int):
-    prem_data.delete_one({'_id': user_id})
+async def unprem(user_id: int):
+    user_data.delete_one({'_id': user_id}, {'$set': {'prem': False}})
     return
-
-async def all_users():
-    user_docs = user_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
-    return user_ids
-
