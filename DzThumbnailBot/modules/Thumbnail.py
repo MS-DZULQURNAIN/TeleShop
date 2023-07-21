6be4b7f79627eb70f46337b4725c5a4b1e1286c3
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-21 10:23:53 +0700

    Create Thumbnail.py

---
+++ DzThumbnailBot/modules/Thumbnail.py
@@ -0,0 +1,27 @@
+import time
+from pyrogram import*
+
+@Dz.on_message(filters.private & (filters.video | filters.document))
+async def thumb_change(bot, m):
+    global thumb
+    await bot.send_message(int(CHANNEL_ID))
+    msg = await m.reply("`Downloading..`")
+    c_time = time.time()
+    file_dl_path = await bot.download_media(message=m, progress=progress_for_pyrogram, progress_args=("Downloading file..", msg, c_time))
+    await msg.delete()
+    answer = await bot.ask(m.chat.id,'Kirimkan foto' + ' untuk dijadikan thumbnail' if thumb else '', filters=filters.photo | filters.text)
+    if answer.photo:
+        try:
+            os.remove(thumb)
+        except:
+            pass
+        thumb = await bot.download_media(message=answer.photo)
+    msg = await m.reply("`Uploading..`")
+    c_time = time.time()
+    done = "Thumbnail by @ThumbnailRobot\n\nChannel: @MSPR0JECT\nSupport: @envSample"
+    if m.document:
+        await bot.send_document(chat_id=m.chat.id, document=file_dl_path, thumb=thumb, caption=done, progress=progress_for_pyrogram, progress_args=("Uploading file..", msg, c_time))
+    elif m.video:
+        await bot.send_video(chat_id=m.chat.id, video=file_dl_path, thumb=thumb, caption=done, progress=progress_for_pyrogram, progress_args=("Uploading file..", msg, c_time))
+    await msg.delete()
+    os.remove(file_dl_path)
