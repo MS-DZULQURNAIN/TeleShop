14e3d918c146a6806c4f890952275c4f148093b8
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-21 09:47:40 +0700

    Create data.py

---
+++ DzThumbnailBot/core/data.py
@@ -0,0 +1,7 @@
+START_TXT = """
+**👋Halo {first}
+
+Saya Adalah {BOT_NAME} yang akan membantu Anda untuk memasang thumbnail video dengan mudah melalui telegram
+
+Klik button tutorial untuk mulai mengetahui cara-cara nya**
+"""
