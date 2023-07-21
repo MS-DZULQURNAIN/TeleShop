228a356d352f804c72ae7c02e363e11f6faa746f
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-21 09:09:54 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,6 +7,12 @@ from plugins.fsub import *
 
 @Dz.on_message(filters.command("start") & filters.private & sub & sub2)
 async def start_cmd(dz: Client, m: Message):
-  try:
-      await add_user
+  id = m.from_user.id
+  if not await cek_user(id):
+    try:
+        await add_user(id)
+      except:
+        pass
+   await m.reply_text(
+     text=START_TXT.format(tag=
   
