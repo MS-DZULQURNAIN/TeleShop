05a9cb71225cfce1f9c6727ed00035cecace8cce
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-21 08:47:19 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,12 @@
+from pyrogram import *
+from pyrogram.types import *
+from pyrogram.enums import *
+from Database.database import *
 
+from plugins.fsub import *
+
+@Dz.on_message(filters.command("start") & filters.private & sub & sub2)
+async def start_cmd(dz: Client, m: Message):
+  try:
+      await add_user
+  
