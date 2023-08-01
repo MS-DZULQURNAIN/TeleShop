295fb12b7be59848584b8c48725f12d3f42028eb
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-02 06:01:29 +0700

    Create config.py

---
+++ TeleShop/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+TOKEN = os.environ.get("TOKEN")
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+LOG = int(os.environ.get("LOG"))
+DATABASE_URI = os.environ.get("DATABASE_URI")
+BOT_NAME = os.environ.get("BOT_NAME")
+thumb = os.environ.get("thumb", "")
